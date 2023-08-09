21b6cfde942e753bc8b407541d8d769c12c36076
Maica <maicasantos90@gmail.com>
2023-08-09 12:47:01 -0700

    test

---
+++ backend/db/models/communities.js
@@ -0,0 +1,45 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Communities extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Communities.belongsTo(
+        models.User,
+          { foreignKey: 'userId' }
+      );
+      Communities.belongsToMany(
+        models.Posts,
+          { through: models.CommunityPosts,
+            foreignKey: 'communityId',
+            otherKey: 'postId'
+          }
+          // additional attributes for the join table can be included in the options
+      );
+      Communities.belongsToMany(
+        models.User,
+          { through: models.CommunityMembers,
+            foreignKey: 'communityId',
+            otherKey: 'userId'
+          }
+          // additional attributes for the join table can be included in the options
+      );
+    }
+  }
+  Communities.init({
+    userId: DataTypes.INTEGER,
+    about: DataTypes.STRING,
+    members: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Communities',
+  });
+  return Communities;
+};
---
+++ backend/db/models/user.js
@@ -0,0 +1,78 @@
+'use strict';
+
+const { Model, Validator } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      User.hasMany(
+        models.Posts,
+          { foreignKey: 'userId', onDelete: 'CASCADE',  hooks: true }
+      );
+      User.hasMany(
+        models.Posts,
+          { foreignKey: 'userId', onDelete: 'CASCADE',  hooks: true }
+      );
+      User.hasMany(
+        models.Communities,
+          { foreignKey: 'userId', onDelete: 'CASCADE',  hooks: true }
+      );
+      User.belongsToMany(
+        models.Communities,
+          { through: models.CommunityMembers,
+            foreignKey: 'userId',
+            otherKey: 'communityId'
+          }
+          // additional attributes for the join table can be included in the options
+      );
+    }
+  }
+  User.init({
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [4, 30],
+        isNotEmail(value) {
+          if (Validator.isEmail(value)) {
+            throw new Error("Cannot be an email.");
+          }
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [3, 256],
+        isEmail: true
+      }
+    },
+    hashedPassword: {
+      type: DataTypes.STRING.BINARY,
+      allowNull: false,
+      validate: {
+        len: [60, 60]
+      }
+    },
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+    defaultScope: {
+      attributes: {
+        exclude: ["hashedPassword", "email", "createdAt", "updatedAt"]
+      }
+    }
+  });
+  return User;
+};
